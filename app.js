206446c5cc4874111044e3562d1281347e2f80c1
Michael <mpkelley08@gmail.com>
2017-10-03 15:03:39 -0400

    Assignment 8 completed

--- app.js
+++ app.js
@@ -1,49 +1,67 @@
 function onReady() {
   var toDos = [];
   var addToDoForm = document.getElementById('addToDoForm');
-  var deleteButton = document.getElementById('deleteBtn');
+
 
   function createNewToDo(){
     var newToDoText = document.getElementById('newToDoText');
+
     toDos.push({
       title: newToDoText.value,
       complete: false
     });
+
     newToDoText.value = '';
 
     renderTheUI(toDos);
   }
 
-  function deleteToDo(toDos){
-    var removeList = document.getElementsByClassName('li1');
-    console.log(removeList[0] + " within the deleteToDo function");
-    console.log(removeList[0].firstElementChild.getAttribute("type") + " within the deleteToDo function");
-    // removeList.forEach(function(remove){
-    //   console.log(removeList + " within the deleteToDo function")
-    //   //console.log(toDo.firstElementChild.getAttribute("checked") + " within the deleteToDo function");
-    //   // if(toDos[toDo].firstChild.checked === true){
-    //   //   toDos[toDo] = '';
-    //   // }
-    // });
+    function deleteToDo(deleteTitle){
+      toDos.forEach(function(toDo) {
+        if(toDo.title === deleteTitle){
+          toDos.splice(toDos.indexOf(toDo), 1);
+        }
 
-  }
+      });
+
+      renderTheUI(toDos);
+    }
+
 
   function renderTheUI(toDos){
-    console.log('hello from renderTheUI: ' + toDos);
+
     var todoList = document.getElementById('toDoList');
 
     toDoList.innerHTML = '';
 
     toDos.forEach(function(toDo) {
       var newLi = document.createElement('li');
-      newLi.setAttribute("class", "li1")
+
+
       var checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+
+      var deleteButton = document.createElement('input');
+      deleteButton.type = "button";
+      deleteButton.value = "Delete To-Do!";
+      deleteButton.setAttribute("id", toDo.title);
 
       newLi.innerHTML = toDo.title;
 
       todoList.appendChild(newLi);
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+
+
+
+
+      //create the Delete button even listener
+      deleteButton.onclick = (function(event) {
+        let deleteTitle = this.getAttribute("id");
+        deleteToDo(deleteTitle);
+      });
+
     });
   }
 
@@ -51,16 +69,9 @@ function onReady() {
   addToDoForm.addEventListener('submit', function(event) {
     event.preventDefault();
     createNewToDo();
+
   });
 
-  //create the Delete button even listener
-  deleteButton.onclick = (function(event) {
-    event.preventDefault();
-    console.log('hello from Delete listener' + toDos);
-    deleteToDo(toDos);
-    //renderTheUI(toDos);
-    //createNewToDo();
-  });
 
   renderTheUI(toDos);
 
